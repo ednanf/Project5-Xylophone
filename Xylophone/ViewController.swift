3458b8371868710b399bca91fbbe29566a725967
Ednan Rogério Frizzera Filho <ednanrff@gmail.com>
2023-08-11 11:27:40 -0300

    lesson 71 completed

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,29 @@
 //
 
 import UIKit
+import AVFoundation // needed to play sounds
 
 class ViewController: UIViewController {
-
+// MARK: - IBOutlets
+    
+// MARK: - Properties
+    var player = AVAudioPlayer()
+    
+// MARK: - Setup
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+// MARK: - IBActions
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound()
+    }
     
-
+// MARK: - Functions
+    func playSound() {
+        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
 }
 
